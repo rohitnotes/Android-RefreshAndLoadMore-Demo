157e8fe2bcd8a362eb13ea46503716e6875ad6ab
TakWolf <takwolf@foxmail.com>
2018-01-02 21:09:48 +0800

    代码整理

--- app/src/main/java/com/takwolf/android/refreshandloadmore/demo/model/AppGlideModule.java
+++ app/src/main/java/com/takwolf/android/refreshandloadmore/demo/model/AppGlideModule.java
@@ -3,4 +3,4 @@ package com.takwolf.android.refreshandloadmore.demo.model;
 import com.bumptech.glide.annotation.GlideModule;
 
 @GlideModule
-public class AppGlideModule extends com.bumptech.glide.module.AppGlideModule {}
+public final class AppGlideModule extends com.bumptech.glide.module.AppGlideModule {}
