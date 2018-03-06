55556ec0899363a221dd79d7d6f6b8f0196ccddb
TakWolf <takwolf@foxmail.com>
2018-03-06 16:59:39 +0800

    补充注解

--- app/src/main/java/com/takwolf/android/refreshandloadmore/demo/ui/adapter/IllustListAdapter.java
+++ app/src/main/java/com/takwolf/android/refreshandloadmore/demo/ui/adapter/IllustListAdapter.java
@@ -41,13 +41,14 @@ public class IllustListAdapter extends RecyclerView.Adapter<IllustListAdapter.Vi
         return illustList.size();
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(inflater.inflate(R.layout.item_illust, parent, false), activity);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(illustList.get(position));
     }
 
--- app/src/main/java/com/takwolf/android/refreshandloadmore/demo/ui/adapter/StoryListAdapter.java
+++ app/src/main/java/com/takwolf/android/refreshandloadmore/demo/ui/adapter/StoryListAdapter.java
@@ -42,13 +42,14 @@ public class StoryListAdapter extends RecyclerView.Adapter<StoryListAdapter.View
         return storyList.size();
     }
 
+    @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
         return new ViewHolder(inflater.inflate(R.layout.item_story, parent, false), activity);
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, int position) {
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         holder.bind(storyList.get(position));
     }
 
